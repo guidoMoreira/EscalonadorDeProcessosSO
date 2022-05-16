0631688c22e383d9d71e2fb4cac2f78b32dc484a
guidoMoreira <48949267+guidoMoreira@users.noreply.github.com>
2022-05-16 20:36:06 -0300

    V0

---
+++ scheduler.c
@@ -0,0 +1,54 @@
+#include "scheduler.h"
+#include "load_jobs.h"
+#include <unistd.h>
+#include <signal.h>
+
+extern void iniciarProcesso(int index);
+
+void scheduler_init(char* jobs, float quantum){
+
+	pidx = 0;
+	alterna = 1;
+	receive=0;	
+
+	loadJobs(jobs);
+
+	print_jobsQueue();
+
+	int i;
+
+	for(i = 0; i < queueSize; i++){
+			iniciarProcesso(i);
+			kill(spid[i], SIGSTOP);
+	}		
+	
+	
+	while(1){	
+		  signal(SIGALRM, alternaTarefa);
+		  alarm(quantum);
+		  while(!receive)
+			pause();
+		  receive = 0; 
+		  
+    }
+
+
+}
+
+
+void alternaTarefa(int signum){	
+	UNUSED(signum);
+
+	receive = 1;	
+	kill(spid[alterna], SIGSTOP);
+	
+	if(!alterna)
+		alterna++;
+	else
+		alterna = 0;	
+	
+	kill(spid[alterna], SIGCONT);
+
+}
+
+
